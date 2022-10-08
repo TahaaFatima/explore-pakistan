4cb3bcbc9c6691bef7bbe88109683a465d19358c
TahaaFatima <55625397+TahaaFatima@users.noreply.github.com>
2022-10-08 12:49:52 +0500

    Update settings.py

--- travelling_company/settings.py
+++ travelling_company/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7#_ucn5kupr4r=_jhus8ks899_kr@pd=@wk*913(h@(dkwxe!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['explore-pakistan.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,10 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = 'static/'
-MEDIA_URL = '/img/'
+# MEDIA_URL = '/img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "explore_pakistan/static")
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
